b55d35d7d0e39868315388e1796d743406beaaa6
twinh <twinhuang@qq.com>
2016-11-18 18:29:42 +0800

    demo增加admin方便admin类迁移 #2028

---
+++ src/Controller/Admin/DemoRecords.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Miaoxing\Demo\Controller\Admin;
+
+use miaoxing\plugin\BaseController;
+
+class DemoRecords extends BaseController
+{
+    public function indexAction()
+    {
+        return $this->suc();
+    }
+}
---
+++ tests/Controller/Admin/DemoRecordsTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MiaoxingTest\Demo\Controller\Admin;
+
+use Miaoxing\Plugin\Test\BaseControllerTestCase;
+
+/**
+ * 后台演示控制器
+ */
+class DemoRecordsTest extends BaseControllerTestCase
+{
+}
